84c99266775d9d4137210d3260365edf26860bc4
Tess Sluijter-Stek <tess@unixerius.nl>
2024-07-12 22:05:01 +0200

    Updated provisioning scripts.

--- Lab setup/provision-vm-x86_64.sh
+++ Lab setup/provision-vm-x86_64.sh
@@ -5,9 +5,14 @@
 
 echo "192.168.56.11   devsecops" >> /etc/hosts
 
+export MYHOME="/home/vagrant"
+
+# Growing the root file system
+lvresize -r -l +40%VG /dev/ubuntu-vg/ubuntu-lv
+
 # Installing pre-requisite software
-apt update
-apt install -y docker.io docker-buildx docker-compose git ripgrep pip python3
+apt-get update
+apt-get install -y docker.io docker-buildx docker-compose git ripgrep pip python3
 
 # Needed for the Cypres tests of JuiceShop
 apt install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2 libxtst6 xauth xvfb
@@ -17,7 +22,7 @@ if [[ $? -ne 0 ]];
 then echo "FAILURE: Docker install failed."; exit 1; fi
 
 # Setting up Java and Maven, for Selenium
-apt install -y openjdk-17-jdk
+apt-get install -y openjdk-17-jdk
 cd ~
 wget https://dlcdn.apache.org/maven/maven-3/3.8.8/binaries/apache-maven-3.8.8-bin.tar.gz
 cd /opt
@@ -39,48 +44,62 @@ then echo "FAILURE: Maven install failed."; exit 1; fi
 
 # Installing NodeJS from outside sources.
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_16.x nodistro main" > /etc/apt/sources.list.d/nodesource.list
-apt update
-apt install -y nodejs      # Fetching NodeJS 16 from NodeSource
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" > /etc/apt/sources.list.d/nodesource.list
+apt-get update
+apt-get install -y nodejs      # Fetching NodeJS 20 from NodeSource
 
 echo "NODE INSTALLED: $(node --version)"
-if [[ $(node --version | cut -d. -f1) != "v16" ]]
+if [[ $(node --version | cut -d. -f1) != "v20" ]]
 then echo "FAILURE: wrong Node version."; exit 1; fi
 
 # Installing Angular
-npm install --location=global @angular/cli@16.2.14
+npm install --location=global @angular/cli
 
-# Installing Chromium, for headless testing.
-apt install -y chromium-browser 
+# Installing Chromium, for headless testing. This uses Snapd
+# And on Hyper-V apparently snapd has a bug.
+apt-get purge -y snapd && apt-get install -y snapd
+if [[ $? -ne 0 ]] 
+then echo "FAILURE: Issues with snapd"; exit 1; fi
+
+apt-get install -y chromium-browser 
+if [[ $? -ne 0 ]] 
+then echo "FAILURE: Issues with Chromium install"; exit 1; fi
+
 echo "export CHROME_BIN=\"/snap/bin/chromium\"" >> /etc/bash.bashrc
 echo "export CHROME_BIN=\"/snap/bin/chromium\"" >> /etc/profile
 
 # Setting up and starting Docker.
-# This assumes that you did a "su" or "sudo -i" to become root.
-usermod -a -G docker $(who am i | cut -d" " -f1)
+usermod -a -G docker vagrant
 systemctl enable docker
 systemctl start docker
 docker pull hello-world
 
 if [[ $? -ne 0 ]];
-then echo "FAILURE: Docker pullfailed."; exit 1; fi
+then echo "FAILURE: Docker pull failed."; exit 1; fi
 
 # Pulling images needed for Selenium labs
+echo "Pulling Selenium container images (two nodes and hub)"
 docker pull selenium/node-chrome:113.0
 docker pull selenium/node-firefox:112.0
 docker pull selenium/hub:4.9
+
+echo "Pulling Juice Shop container image"
 docker pull bkimminich/juice-shop:v15.0.0
 
-# Pulling image for SCA with OSV Scanner and Trivy
+# Pulling image for SCA with OSV Scanner
+echo "Pulling OSV Scanner container image"
 docker pull ghcr.io/google/osv-scanner:latest
-docker pull aquasec/trivy
 
 # Pulling Nessus image for day 5
+echo "Pulling Nessus Ubuntu container image"
 docker pull tenable/nessus:latest-ubuntu
-mkdir ${HOME}/Nessus
-curl --request GET --url "https://raw.githubusercontent.com/unixerius/DSO/main/Nessus/nessus-docker-compose.yml" --output "${HOME}/Nessus/docker-compose.yml" 
+mkdir ${MYHOME}/Nessus
+curl --request GET --url "https://raw.githubusercontent.com/unixerius/DSO/main/Nessus/nessus-docker-compose.yml" --output "${MYHOME}/Nessus/docker-compose.yml" 
 
 # Pulling zap2docker and Nuclei, which are big too
+echo "Pulling Zaproxy container image"
 docker pull ghcr.io/zaproxy/zaproxy:latest
+echo "Pulling Nuclei container image"
 docker pull projectdiscovery/nuclei
- 
+
+chown -R vagrant:vagrant ${MYHOME}
